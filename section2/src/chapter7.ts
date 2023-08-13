d54bffadeac354316b740a58bfc590be0eb209e1
jungwoo3490 <jungwoo3490@naver.com>
2023-08-14 02:44:55 +0900

    Void와 Never 타입

--- section2/src/chapter7.ts
+++ section2/src/chapter7.ts
@@ -10,5 +10,14 @@ function func2(): void {
     console.log("hello");
 }
 
-let a: void;
-a = undefined;
+// never
+// never -> 존재하지 않는
+// 불가능한 타입
+
+function func3(): never {
+    while (true) {} 
+}
+
+function func4(): never {
+    throw new Error();
+}
